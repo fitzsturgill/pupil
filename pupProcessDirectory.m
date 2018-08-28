cf4afa4c908969128f99f87b862052988e85d1e4
Fitz Sturgill <fitzsturgill@gmail.com>
2018-08-28 18:05:35 -0400

    new version of pupProcessDirectory allows there to be missing file numbers within a range of file numbers, see header for use

--- pupProcessDirectory.m
+++ pupProcessDirectory.m
@@ -1,4 +1,4 @@
-function pupProcessDirectory(start, showProgress)
+function pupProcessDirectory(start, showProgress, stop)
     if nargin < 1
         start = 0; % assuming that bonsai is saving first video with suffix 0 (zero)
     end
@@ -6,12 +6,29 @@ function pupProcessDirectory(start, showProgress)
     if nargin < 2
         showProgress = 1;
     end
-    global state
     
-    fileNumber = start;    
-    while pupLoadVideo(fileNumber)
-        pupProcessVideo(showProgress);
-        pupSaveData;
-        fileNumber = fileNumber + 1;
-        drawnow;
+    if nargin < 3
+        stop = 500;
     end
+    
+    for fileNumber = start:stop 
+        if pupLoadVideo(fileNumber)        
+            pupProcessVideo(showProgress);
+            pupSaveData;
+            drawnow;
+        end
+    end
+    
+    
+    
+    %% old version
+%         fileNumber = start;    
+%     while pupLoadVideo(fileNumber)        
+%         pupProcessVideo(showProgress);
+%         pupSaveData;
+%         fileNumber = fileNumber + 1;
+%         drawnow;
+%         if fileNumber == stop
+%             break
+%         end
+%     end
